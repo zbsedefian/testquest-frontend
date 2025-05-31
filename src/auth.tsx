35f9d3e56d2d726c3357585a5404aa68ec4acd68
Zachary Sedefian <zbsedefian@gmail.com>
2025-05-30 21:06:29 -0400

    type ReactNode

--- src/auth.tsx
+++ src/auth.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, ReactNode } from "react";
+import React, { createContext, useContext, useState, type ReactNode } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 
 export interface User {
